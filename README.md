7fe931cf1759708a6e2ce71807103f943fcdafea
GluayGG <ratapops@gmail.com>
2022-06-09 11:27:14 +0700

    test

---
+++ README.md
@@ -0,0 +1,7 @@
+This is my test project :)
+
+<a href="https://projects.rpzone.space" target="_blank">Website</a>
+
+- Nextjs
+- Tailwindcss
+- Firebase
---
+++ pages/index.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import { createFirebaseApp } from "../firebase/clientApp";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { useEffect } from "react";
+
+const app = createFirebaseApp();
+
+const getUser = async () => {
+  const db = getFirestore(app);
+  const docRef = doc(db, "users", "aIj08MaLmoMOXTDdwQBB");
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+};
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Head>
+          <title>GG - Projects</title>
+          <link rel="icon" href="favicon.ico" />
+        </Head>
+      </div>
+
+      <div className="h-screen text-center bg-gradient-to-r from-cyan-500 to-blue-500  py-8">
+        <h1 className="text-2xl font-bold">
+          Hello everyone please select one of my projects :)
+        </h1>
+
+        <div className="flex flex-col items-center">
+          <button
+            type="button"
+            className=" border rounded my-4 py-2 bg-white hover:bg-orange-400"
+            onClick={getUser}
+          >
+            01
+          </button>
+          <button
+            type="button"
+            className=" border rounded my-4 py-2 bg-white hover:bg-orange-400"
+          >
+            02
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
